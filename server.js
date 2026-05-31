3848071e7f9302db80c14e84f3a92d6a2c45335e
hareshkumar-u <77015851+hareshkumar-u@users.noreply.github.com>
2026-05-31 08:21:58 -0500

    Removed ngrok

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const multer = require('multer');
 const path = require('path');
 const fs = require('fs');
-const ngrok = require('@ngrok/ngrok');
 
 const app = express();
 const PORT = 3000;
